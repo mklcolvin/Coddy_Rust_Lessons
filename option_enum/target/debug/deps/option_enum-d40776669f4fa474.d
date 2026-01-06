b0de87dc3ce8fdf01b850fd89fb3bfaa44ef4886
mklcolvin <mklcolvin@gmail.com>
2026-01-06 13:20:05 -0600

    More updates

--- option_enum/target/debug/deps/option_enum-d40776669f4fa474.d
+++ option_enum/target/debug/deps/option_enum-d40776669f4fa474.d
@@ -1,5 +1,6 @@
-/home/mklcolvin/Documents/Programming/Rust/Coddy_Rust_Lessons/option_enum/target/debug/deps/option_enum-d40776669f4fa474.d: src/main.rs
+/home/mklcolvin/Documents/Programming/Rust/Coddy_Rust_Lessons/option_enum/target/debug/deps/option_enum-d40776669f4fa474.d: src/main.rs src/profile.rs
 
-/home/mklcolvin/Documents/Programming/Rust/Coddy_Rust_Lessons/option_enum/target/debug/deps/liboption_enum-d40776669f4fa474.rmeta: src/main.rs
+/home/mklcolvin/Documents/Programming/Rust/Coddy_Rust_Lessons/option_enum/target/debug/deps/liboption_enum-d40776669f4fa474.rmeta: src/main.rs src/profile.rs
 
 src/main.rs:
+src/profile.rs:
